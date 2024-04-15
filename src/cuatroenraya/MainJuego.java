19066c60b461e9c9096822e06ac4ac35d208f8df
jSolerMulet <166610250+jSolerMulet@users.noreply.github.com>
2024-04-15 11:39:40 +0200

    Update MainJuego.java

--- src/cuatroenraya/MainJuego.java
+++ src/cuatroenraya/MainJuego.java
@@ -3,8 +3,9 @@ package cuatroenraya;
 public class MainJuego {
 
     public static void main(String[] args) {
-        
+        // Creamos un objeto juego para poder ejecutar el programa
     	JuegoNoEjecutable juego = new JuegoNoEjecutable();
+        // Llamamos al metodo que inicia el juego de la clase no ejecutable
         juego.iniciarJuego();
     }
 }
